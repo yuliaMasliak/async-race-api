54767ef573da2d510cd6dfa0478a4d45f29036b3
Yulia Masliak <1985kozak@gmail.com>
2023-06-06 11:50:30 +0300

    fix: fix set header

--- async-race-api/index.js
+++ async-race-api/index.js
@@ -39,7 +39,7 @@ app.use(express.json());
 const state = { velocity: {}, blocked: {} };
 
 app.use('/garage', (req, res, next) => {
-  res.send({ 'X-Total-Count': `${garage.length}` });
+  res.setHeader({ 'X-Total-Count': `${garage.length}` });
   next();
 });
 
